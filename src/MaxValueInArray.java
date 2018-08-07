3b25bd313245be6c7327ad048e30ea93b6f84a15
longsutls <longsutls@gmail.com>
2018-08-07 09:23:14 +0700

    Enter size of array

---
+++ src/MaxValueInArray.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class MaxValueInArray {
+    public static void main(String[] args) {
+        int size;
+        int[] array;
+        Scanner scanner = new Scanner(System.in);
+
+        do {
+            System.out.print("Enter a size: ");
+            size = scanner.nextInt();
+            if (size > 20)
+                System.out.println("Size should not exceed 20");
+        } while (size > 20);
+    }
+}
