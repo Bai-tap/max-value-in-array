d1c447e5b0f70b9a040e137ace0910aff864e8ca
longsutls <longsutls@gmail.com>
2018-08-07 09:26:04 +0700

    Enter elements

--- src/MaxValueInArray.java
+++ src/MaxValueInArray.java
@@ -12,5 +12,13 @@ public class MaxValueInArray {
             if (size > 20)
                 System.out.println("Size should not exceed 20");
         } while (size > 20);
+
+        array = new int[size];
+        int i = 0;
+        while (i < array.length) {
+            System.out.print("Enter element " + (i + 1) + " : ");
+            array[i] = scanner.nextInt();
+            i++;
+        }
     }
 }
